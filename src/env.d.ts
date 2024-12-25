892e9cd990accc010ecc973b7221165d06439bb1
tosirinpa <53685546+tosirinpa@users.noreply.github.com>
2024-12-26 02:42:04 +0900

    � fix: 環境変数の型定義を追加
    Astroプロジェクトで環境変数を正しく認識するために、
    env.d.tsファイルに必要な型定義を追加しました。
    
    - NOTION_API_SECRET
    - DATABASE_ID

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NOTION_API_SECRET: string;
+  readonly DATABASE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
